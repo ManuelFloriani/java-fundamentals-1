5d01aa807ec2ecc9531490f7cb736148c63ecdc7
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-10-18 14:52:23 +0200

    Correction first excercise: FizzBuzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -3,7 +3,7 @@ package org.lessons.java;
 public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0){
+            if (i % 3 == 0 && i % 5 == 0){
                 System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
